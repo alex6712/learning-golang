c1163a4a5f92a895a295d3f2ad0a0bf4c67b8777
alex6712 <alexeivanyukov@yandex.ru>
2025-05-22 23:34:24 +0300

    Проверка runtime

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	start := time.Now()
+	for x := 0; x < 1_000_000_000; x++ {
+	}
+	elapsed := time.Since(start).Seconds()
+
+	fmt.Printf("Миллиард итераций: %.2f секунд\n", elapsed)
 }
