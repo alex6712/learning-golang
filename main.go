0c2a2bc6a711a7a4593a8510f4c5b4afcdea2914
alex6712 <alexeivanyukov@yandex.ru>
2025-05-27 22:29:05 +0300

    Array и slice

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 )
 
 func main() {
-	i := 1
-	p := &i
+	numbers := [3]int{1, 2, 3}
+	numbersPointer := &numbers
 
-	*p++
+	fmt.Println(numbers)
 
-	fmt.Printf("p = %x\n", p)
-	fmt.Printf("i = %d, *p = %d\n", i, *p)
+	firstSlice := numbers[:2]
+	secondSlice := numbers[1:]
+
+	fmt.Println(firstSlice, secondSlice)
+
+	numbersPointer[1] = 0
+
+	fmt.Println(firstSlice, secondSlice)
+	fmt.Println(numbers)
 }
