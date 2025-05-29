e6f0da9ae5d8dc2203742890809f77c67e6baeca
alex6712 <alexeivanyukov@yandex.ru>
2025-05-29 22:19:58 +0300

    Первый сервер Gin

--- main.go
+++ main.go
@@ -1,22 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
+
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
 func main() {
-	numbers := [3]int{1, 2, 3}
-	numbersPointer := &numbers
+    router := gin.Default()
+    router.GET("/albums", getAlbums)
+    router.GET("/albums/:id", getAlbumByID)
+    router.POST("/albums", postAlbums)
 
-	fmt.Println(numbers)
+    router.Run("localhost:8080")
+}
 
-	firstSlice := numbers[:2]
-	secondSlice := numbers[1:]
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, albums)
+}
 
-	fmt.Println(firstSlice, secondSlice)
+func postAlbums(c *gin.Context) {
+    var newAlbum album
 
-	numbersPointer[1] = 0
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
 
-	fmt.Println(firstSlice, secondSlice)
-	fmt.Println(numbers)
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+    id := c.Param("id")
+
+    for _, album := range albums {
+        if album.ID == id {
+            c.IndentedJSON(http.StatusOK, album)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
