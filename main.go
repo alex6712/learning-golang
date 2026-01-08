5e1fd6dc23852324be4e3a7197051bf65e6748a7
alex6712 <alexeivanyukov@yandex.ru>
2026-01-08 16:00:25 +0300

    Работа со слайсами

--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	X, Y int
-}
-
-var (
-	v1 = Vertex{1, 2}
-	v2 = Vertex{X: 1}
-	v3 = Vertex{}
-	p  = &Vertex{1, 2}
-)
-
 func main() {
-	fmt.Println(v1, p, v2, v3)
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i)
+	}
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
 }
