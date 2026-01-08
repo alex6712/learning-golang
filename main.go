28983f67e343c8b9e1f0287790236936518ba6d2
alex6712 <alexeivanyukov@yandex.ru>
2026-01-08 16:50:31 +0300

    Начало работы с методами

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func fibonacci() func() int {
-	prev, cur := 1, 0
+type Vertex struct {
+	X, Y float64
+}
 
-	return func() int {
-		prev, cur = cur, prev+cur
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
 
-		return cur
-	}
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
 }
 
 func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
-	}
+	v := Vertex{3, 4}
+	v.Scale(10)
+
+	p := &v
+	p.Scale(0.5)
+
+	fmt.Println(v.Abs())
 }
