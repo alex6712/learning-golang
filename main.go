3af57d73869cb6ecba19ed9a5eb6aa652315b34a
alex6712 <alexeivanyukov@yandex.ru>
2026-01-12 17:09:26 +0300

    Упражнение Equivalent Binary Trees (Concurrency: 8)

--- main.go
+++ main.go
@@ -1,24 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type List[T any] struct {
-	next *List[T]
-	val  T
+	"golang.org/x/tour/tree"
+)
+
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+
+	ch <- t.Value
+
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
+
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for range 10 {
+		if <-ch1 != <-ch2 {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
-	head := &List[int]{val: 0}
-	tail := head
-
-	for i := 1; i < 10; i++ {
-		new := &List[int]{val: i}
-
-		tail.next = new
-		tail = new
-	}
-
-	for current := head; current != nil; current = current.next {
-		fmt.Printf("New node found: val=%v\n", current.val)
-	}
+	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
