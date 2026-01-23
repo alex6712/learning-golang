02d4a40231c3edc67cbd8571ef10ab4861b51989
alex6712 <alexeivanyukov@yandex.ru>
2026-01-23 14:58:56 +0300

    Улучшенная версия crawler

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 type SafeCache struct {
@@ -15,18 +14,18 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
-func Crawl(url string, depth int, fetcher Fetcher, cache *SafeCache) {
+func Crawl(url string, depth int, fetcher Fetcher, cache *SafeCache, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	if depth <= 0 {
 		return
 	}
 
 	cache.mu.Lock()
-
 	visited := cache.v[url]
 	if !visited {
 		cache.v[url] = true
 	}
-
 	cache.mu.Unlock()
 
 	if visited {
@@ -39,16 +38,21 @@ func Crawl(url string, depth int, fetcher Fetcher, cache *SafeCache) {
 		return
 	}
 	fmt.Printf("found: %s %q\n", url, body)
-
 	for _, u := range urls {
-		go Crawl(u, depth-1, fetcher, cache)
+		wg.Add(1)
+		go Crawl(u, depth-1, fetcher, cache, wg)
 	}
 }
 
 func main() {
 	cache := SafeCache{v: make(map[string]bool)}
-	go Crawl("https://golang.org/", 4, fetcher, &cache)
-	time.Sleep(time.Second)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go Crawl("https://golang.org/", 4, fetcher, &cache, &wg)
+
+	wg.Wait()
 }
 
 type fakeFetcher map[string]*fakeResult
