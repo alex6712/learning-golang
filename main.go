3991e425bf47e197f6ddc1083d30553afa6d384a
alex6712 <alexeivanyukov@yandex.ru>
2026-01-23 14:30:08 +0300

    Упражнение Web Crawler (Concurrency: 10)

--- main.go
+++ main.go
@@ -2,42 +2,98 @@ package main
 
 import (
 	"fmt"
-
-	"golang.org/x/tour/tree"
+	"sync"
+	"time"
 )
 
-// Walk проходит по бинарному отсортированному
-// дереву t и пушит значения поэлементно в канал ch
-func Walk(t *tree.Tree, ch chan int) {
-	if t == nil {
+type SafeCache struct {
+	mu sync.Mutex
+	v  map[string]bool
+}
+
+type Fetcher interface {
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+func Crawl(url string, depth int, fetcher Fetcher, cache *SafeCache) {
+	if depth <= 0 {
 		return
 	}
 
-	Walk(t.Left, ch)
-	ch <- t.Value
-	Walk(t.Right, ch)
-}
+	cache.mu.Lock()
 
-// Same проверяет, совпадают ли два отсортированных
-// бинарных дерева t1 и t2
-func Same(t1, t2 *tree.Tree) bool {
-	ch1 := make(chan int, 10)
-	ch2 := make(chan int, 10)
-
-	// запускаем проход по деревьям в отдельных потоках
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
-
-	for range 10 {
-		// main поток блокируется, пока не получит
-		// по элементу из каждого канала, потом их сравнивает
-		if <-ch1 != <-ch2 {
-			return false
-		}
+	visited := cache.v[url]
+	if !visited {
+		cache.v[url] = true
 	}
-	return true
+
+	cache.mu.Unlock()
+
+	if visited {
+		return
+	}
+
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+
+	for _, u := range urls {
+		go Crawl(u, depth-1, fetcher, cache)
+	}
 }
 
 func main() {
-	fmt.Println(Same(tree.New(3), tree.New(3)))
+	cache := SafeCache{v: make(map[string]bool)}
+	go Crawl("https://golang.org/", 4, fetcher, &cache)
+	time.Sleep(time.Second)
+}
+
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
 }
